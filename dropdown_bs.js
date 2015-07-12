9c959f89488bc02013342b767f8199283037880e
yapcheahshen <yapcheahshen@gmail.com>
2015-07-12 12:19:39 +0800

    first working version

---
+++ dropdown_bs.js
@@ -0,0 +1,32 @@
+var React=require("react/addons");
+var E=React.createElement;
+var PT=React.PropTypes;
+
+var Button=require("react-bootstrap").Button;
+var DropdownButton=require("react-bootstrap").DropdownButton;
+var MenuItem=require("react-bootstrap").MenuItem;
+
+var BreadCrumbDropdown=React.createClass({
+	mixins:[React.addons.pureRenderMixin]
+	,propTypes:{
+		items:PT.array.isRequired
+		,selected:PT.number
+		,onSelect:PT.func
+		,level:PT.number.isRequired //which level
+	}
+	,getDefaultProp:function(){
+		return {items:[]}
+	}
+	,onSelect:function(idx) {
+		this.props.onSelect&&this.props.onSelect(idx,this.props.items,this.props.level);
+	}
+	,renderItem:function(item,idx) {
+		return E(MenuItem,{key:idx,active:this.props.selected==idx,eventKey:idx},item.t);
+	}
+	,render:function(){
+		var title=this.props.items[this.props.selected].t;
+		return E(DropdownButton,{onSelect:this.onSelect,noCaret:true,title:title},
+			this.props.items.map(this.renderItem));
+	}
+});
+module.exports=BreadCrumbDropdown;
---
+++ theme_bootstrap.js
@@ -0,0 +1,3 @@
+var ButtonGroup=require("react-bootstrap").ButtonGroup;
+var BreadCrumbDropdown=require("./dropdown_bs");
+module.exports={container:ButtonGroup,dropdown:BreadCrumbDropdown};
