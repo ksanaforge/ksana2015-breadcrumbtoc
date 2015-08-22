300ef3d3f4a14f934854aa9f14e9d7f384f1ae3c
yapcheahshen <yapcheahshen@gmail.com>
2015-08-22 19:44:27 +0800

    cbeta toc ok, bump to 1.0.1

---
+++ breadcrumbtoc.js
@@ -0,0 +1,102 @@
+var React=require("react/addons");
+var E=React.createElement;
+var PT=React.PropTypes;
+var buildToc = function(toc) {
+	if (!toc || !toc.length || toc.built) return;
+	var depths=[];
+ 	var prev=0;
+ 	for (var i=0;i<toc.length;i++) delete toc[i].n;
+	for (var i=0;i<toc.length;i++) {
+	    var depth=toc[i].d||toc[i].depth;
+	    if (prev>depth) { //link to prev sibling
+	      if (depths[depth]) toc[depths[depth]].n = i;
+	      for (var j=depth;j<prev;j++) depths[j]=0;
+	    }
+    	depths[depth]=i;
+    	prev=depth;
+	}
+	toc.built=true;
+	return toc;
+}
+var getChildren = function(toc,n) {
+	if (!toc[n]||!toc[n+1] ||toc[n+1].d!==toc[n].d+1) return [];
+	var out=[],next=n+1;
+	while (next) {
+		out.push(next);
+		if (!toc[next+1])break;
+		if (toc[next].d==toc[next+1].d) {
+			next++;
+		} else if (toc[next].n){
+			next=toc[next].n;			
+		} else {
+			next=0;
+		}
+	}
+	return out;
+}
+var BreadcrumbTOC=React.createClass({
+	mixins:[React.addons.pureRenderMixin]
+	,propTypes:{
+		toc:PT.array.isRequired
+		,hits:PT.array
+		,theme:PT.object
+		,onSelect:PT.func
+		,vpos:PT.number  //jump with vpos
+		,keyword:PT.string
+	}
+	,componentWillReceiveProps:function(nextProps,nextState) {
+		if (nextProps.toc && !nextProps.toc.built) {
+			buildToc(nextProps.toc);
+		}
+		if (nextProps.hits!==this.props.hits) {
+			this.clearHits();
+		}
+	}
+	,componentWillMount:function(){
+		buildToc(this.props.toc);
+	}
+	,getDefaultProps:function() {
+		return {theme:{}};
+	}
+	,getInitialState:function(){
+		return {};
+	}
+	,clearHits:function() {
+		for (var i=0;i<this.props.toc;i++) {
+			if (this.props.toc[i].hit) delete this.props.toc[i].hit;
+		}
+	}
+	,onSelect:function(idx,children,level) {
+		this.props.onSelect && this.props.onSelect(idx, children[idx].vpos+1);//don't know why???
+	}
+	,closestItem:function(tocitems,vpos) {
+		for (i=1;i<tocitems.length;i++) {
+			if (this.props.toc[tocitems[i]].vpos>=vpos) return i-1;
+		}
+		return tocitems.length-1;
+	}
+	,renderCrumbs:function() {
+		var dropdown=this.props.theme.dropdown;
+		var cur=0,toc=this.props.toc,out=[],level=0;
+		do {
+			var children=getChildren(toc,cur);
+			if (!children.length) break;
+
+			var selected = this.closestItem(children,this.props.vpos) ;
+			cur=children[selected];
+			var items=children.map(function(child){
+				return {t:toc[child].t,idx:child,hit:toc[child].hit,vpos:toc[child].vpos};
+			});
+			out.push(E(dropdown,{onSelect:this.onSelect,level:level,
+				key:out.length,selected:selected,items:items,keyword:this.props.keyword}) );
+			//if (out.length>5) break;
+			level++;
+		} while (true);
+		return out;
+	}
+	,render:function(){
+		var container=this.props.theme.container || "div";
+		return E(container,null,this.renderCrumbs());
+	}
+})
+module.exports=BreadcrumbTOC;
--- dropdown_bs.js
+++ dropdown_bs.js
@@ -13,6 +13,7 @@ var BreadCrumbDropdown=React.createClass({
 		,selected:PT.number
 		,onSelect:PT.func
 		,level:PT.number.isRequired //which level
+		,keyword:PT.string
 	}
 	,getDefaultProp:function(){
 		return {items:[]}
@@ -20,16 +21,30 @@ var BreadCrumbDropdown=React.createClass({
 	,onSelect:function(idx) {
 		this.props.onSelect&&this.props.onSelect(idx,this.props.items,this.props.level);
 	}
+	,renderKeyword:function(t) {
+		if (this.props.keyword) {
+			var o=[],lastidx=0;
+			t.replace(new RegExp(this.props.keyword,"g"),function(m,idx){
+				o.push(t.substr(lastidx,idx));
+				o.push(E("span",{key:idx,style:{color:"red"}},m));
+				lastidx=idx+m.length;
+			});
+			o.push(t.substr(lastidx));
+			t=o;
+		}
+		return t;
+	}
 	,renderItem:function(item,idx) {
 		var hit=null;
 		item.hit&&(hit=E("span",{className:"hl0 pull-right"},item.hit));
-		return E(MenuItem,{key:idx,active:this.props.selected==idx,eventKey:idx},item.t,hit);
+		var t=this.renderKeyword(item.t);
+		return E(MenuItem,{key:idx,active:this.props.selected==idx,eventKey:idx},t,hit);
 	}
 	,render:function(){
 		var item=this.props.items[this.props.selected];
 		var title=item.t;
 		item.hit&&(title=[E("span",{key:1},item.t),E("span",{key:2,className:"hl0 pull-right"},item.hit||"")]);
-		return E(DropdownButton,{onSelect:this.onSelect,noCaret:true,title:title},
+		return E(DropdownButton,{onSelect:this.onSelect,noCaret:true,title:this.renderKeyword(title)},
 			this.props.items.map(this.renderItem));
 	}
 });
